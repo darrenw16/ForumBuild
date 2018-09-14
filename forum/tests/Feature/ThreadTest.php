53f91e77dada0464fb58f7fa2c29bfec8e410700
Darren Walsh <darrenw@loveagency.com.au>
2018-09-14 13:56:02 +1000

    lesson 2 complete

---
+++ forum/tests/Feature/ThreadTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ThreadTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function a_user_can_browse_threads()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads');
+
+        $response->assertSee($thread->title);
+    }
+
+
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' .$thread->id);
+
+        $response->assertSee($thread->title);
+    }
+}
