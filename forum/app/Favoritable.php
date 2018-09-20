3a080e4f3c23bdcb77b999136f8706b31ce75d5a
Darren Walsh <darrenw@loveagency.com.au>
2018-09-20 13:55:23 +1000

    lesson 35

--- forum/app/Favoritable.php
+++ forum/app/Favoritable.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 trait Favoritable
 {
+
+    protected static function bootFavoritable()
+    {
+        static::deleting(function ($model) {
+            $model->favorites->each->delete();
+        });
+    }
     /**
      * A reply can be favorited.
      *
@@ -35,7 +42,7 @@ trait Favoritable
     public function unfavorite()
     {
         $attributes = ['user_id' => auth()->id()];
-        $this->favorites()->where($attributes)->delete();
+        $this->favorites()->where($attributes)->get()->each->delete();
     }
 
     /**
